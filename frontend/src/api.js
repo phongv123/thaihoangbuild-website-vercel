423ff0be2cb7aa4f6dd77b6db5f26a2262b795aa
phongv123 <thaivinhphong2305@gmail.com>
2026-08-15 15:13:54 +0700

    Fix url backend in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
-import axios from 'axios'
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api'
+import axios from 'axios';
 
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
+
+export default api;
